7ea14640513ebd1d7c099fb0c4f8d673f856eb08
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-02 23:20:23 +0200

    chore: optimize test of app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  });
+
   it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it("should match the snapshot", () => {
+    expect(fixture).toMatchSnapshot();
   });
 });
