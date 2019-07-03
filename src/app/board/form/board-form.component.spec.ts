d7182a77430551fb7a8887e7c8056264b516b447
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-03 21:20:42 +0200

    feat: handle changes of the board input binding

--- src/app/board/form/board-form.component.spec.ts
+++ src/app/board/form/board-form.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { BoardFormComponent } from "./board-form.component";
 import { BoardFormBuilderService } from "./board-form-builder.service";
 import { ReactiveFormsModule } from "@angular/forms";
+import { boardFactory } from "src/app/shared/board/testing/board.mock";
+import { SimpleChange, SimpleChanges } from "@angular/core";
+import { Board } from "src/app/shared/board/board.model";
+import { Board } from "src/app/shared/board/board.model";
 
 describe("BoardFormComponent", () => {
   let component: BoardFormComponent;
@@ -28,5 +32,19 @@ describe("BoardFormComponent", () => {
 
   it("should match the snapshot", () => {
     expect(fixture).toMatchSnapshot();
+  });
+
+  describe("ngOnChanges", () => {
+    it("should set the board property", () => {
+      const changedBoard: Board = boardFactory();
+      const changes: SimpleChanges = {
+        board: new SimpleChange(null, changedBoard, true)
+      };
+      component.board = null;
+
+      component.ngOnChanges(changes);
+
+      expect(component.board).toBe(changedBoard);
+    });
   });
 });
--- src/app/board/form/board-form.component.ts
+++ src/app/board/form/board-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectionStrategy
+} from "@angular/core";
 import { Board } from "src/app/shared/board/board.model";
 import { BoardFormBuilderService } from "./board-form-builder.service";
 import { FormGroup } from "@angular/forms";
@@ -6,13 +13,20 @@ import { FormGroup } from "@angular/forms";
 @Component({
   selector: "app-board-form",
   templateUrl: "./board-form.component.html",
-  styleUrls: ["./board-form.component.scss"]
+  styleUrls: ["./board-form.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BoardFormComponent {
+export class BoardFormComponent implements OnChanges {
   @Input()
   board: Board;
 
   formGroup: FormGroup = this.formBuilder.initial();
 
   constructor(private formBuilder: BoardFormBuilderService) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.board) {
+      this.board = changes.board.currentValue;
+    }
+  }
 }
