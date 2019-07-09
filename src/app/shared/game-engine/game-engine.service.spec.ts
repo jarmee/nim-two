3c56d9bbce8d6500ded4be8f84c8b69991907667
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-09 19:46:26 +0200

    chore: add further tests

--- src/app/shared/game-engine/game-engine.service.spec.ts
+++ src/app/shared/game-engine/game-engine.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 import * as faker from "faker";
+import { BoardBuilder } from "../board/board.builder";
 import { Board, BoardDifferences } from "../board/board.model";
 import { boardFactory } from "../board/testing/board.mock";
 import { GameState, GameStatus, GAME_STATE_STORE } from "./game-engine.model";
@@ -73,25 +74,24 @@ describe("GameEngineService", () => {
     });
   });
 
+  describe("status$", () => {
+    it("should emit the current game status", done => {
+      service.status$.subscribe((actualStatus: GameStatus) => {
+        expect(actualStatus).toBe(store.snapshot.status);
+        done();
+      });
+    });
+  });
+
   describe("diff", () => {
     it("should return an empty array if both Board objects have the same values", () => {
-      const newBoard: Board = {
-        0: {
-          0: {
-            value: false,
-            player: faker.name.firstName()
-          }
-        }
-      };
+      const newBoard: Board = BoardBuilder.create()
+        .addRowWithColumns(false)
+        .build();
 
-      const currentBoard: Board = {
-        0: {
-          0: {
-            value: false,
-            player: faker.name.firstName()
-          }
-        }
-      };
+      const currentBoard: Board = BoardBuilder.create()
+        .addRowWithColumns(false)
+        .build();
 
       const actual = diff(newBoard, currentBoard);
 
@@ -99,14 +99,9 @@ describe("GameEngineService", () => {
     });
 
     it("should return an emptry array if one of the parameter is null", () => {
-      const newBoard = {
-        0: {
-          0: {
-            value: true,
-            player: faker.name.firstName()
-          }
-        }
-      };
+      const newBoard = BoardBuilder.create()
+        .addRowWithColumns(true)
+        .build();
       const currentBoard = null;
 
       const actual = diff(newBoard, currentBoard);
@@ -141,51 +136,47 @@ describe("GameEngineService", () => {
   });
 
   describe("checkRules", () => {
-    const newColumn = {
-      value: false,
-      player: faker.name.firstName()
-    };
-    const newBoard: Board = {
-      0: {
-        0: newColumn
-      }
-    };
-    const currentColumn = {
-      value: true,
-      player: faker.name.firstName()
-    };
-    const currentBoard: Board = {
-      0: {
-        0: currentColumn
-      }
-    };
+    const newState: GameState = gameStateFactory
+      .extend({
+        board: BoardBuilder.create()
+          .addRowWithColumns(true)
+          .build()
+      })
+      .build();
+    const actualState: GameState = gameStateFactory
+      .extend({
+        board: BoardBuilder.create()
+          .addRowWithColumns(false)
+          .build()
+      })
+      .build();
     const boardDifferences: BoardDifferences = [
       {
-        newColumn,
-        currentColumn,
+        newColumn: newState.board[0][0],
+        currentColumn: actualState.board[0][0],
         path: ["0", "0"]
       }
     ];
 
-    it("should return the provided newBoard if no rules are given", () => {
-      const actual = checkRules([])(newBoard, currentBoard, boardDifferences);
+    it("should return the provided newState if no rules are given", () => {
+      const actual = checkRules([])(newState, actualState, boardDifferences);
 
-      expect(actual).toEqual(newBoard);
+      expect(actual).toEqual(newState);
     });
 
     it("should call the provided rule", () => {
-      const rule = jest.fn().mockReturnValue(state => newBoard);
+      const rule = jest.fn().mockReturnValue(state => newState);
 
       const actual = checkRules([rule])(
-        newBoard,
-        currentBoard,
+        newState,
+        actualState,
         boardDifferences
       );
 
-      expect(actual).toEqual(newBoard);
+      expect(actual).toEqual(newState);
       expect(rule).toHaveBeenCalledWith(
-        newBoard,
-        currentBoard,
+        newState,
+        actualState,
         boardDifferences
       );
     });
