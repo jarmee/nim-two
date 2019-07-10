916514f90e8446c25db39bc686c82cf101d47420
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-10 21:06:22 +0200

    chore: add missing reset test to the game engine store

--- src/app/shared/game-engine/game-engine.store.spec.ts
+++ src/app/shared/game-engine/game-engine.store.spec.ts
@@ -1,5 +1,5 @@
 import { skip } from "rxjs/operators";
-import { BoardBuilder } from '../board/board.builder';
+import { BoardBuilder } from "../board/board.builder";
 import { GameState, GameStatus } from "./game-engine.model";
 import { GameEngineStore } from "./game-engine.store";
 import { gameStateFactory } from "./testing/game-engine.mock";
@@ -7,27 +7,27 @@ import { gameStateFactory } from "./testing/game-engine.mock";
 describe("GameEngineStore", () => {
   const initialGameState: GameState = gameStateFactory.build();
   const updatedState = gameStateFactory
-  .extend({
-    board: BoardBuilder.create()
-    .addRowWithColumns(
-      true,
-      true,
-      true,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false
-    )
-    .build(),
-    status: GameStatus.HumanPlay
-  })
-  .build();
+    .extend({
+      board: BoardBuilder.create()
+        .addRowWithColumns(
+          true,
+          true,
+          true,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        )
+        .build(),
+      status: GameStatus.HumanPlay
+    })
+    .build();
   let store: GameEngineStore;
 
   beforeEach(() => {
@@ -54,8 +54,13 @@ describe("GameEngineStore", () => {
   });
 
   describe("reset", () => {
-    it("should set the state to the initial state", () => {
-      store.next();
+    it("should set the state to the initial state", done => {
+      store.pipe(skip(1)).subscribe((actualState: GameState) => {
+        expect(actualState).toEqual(initialGameState);
+        done();
+      });
+
+      store.reset();
     });
   });
 });
