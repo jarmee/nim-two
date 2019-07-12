bd74724e2bc1e3262912f93c4f6cdb80b4566861
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-12 20:09:01 +0200

    feat: add players$ selector

--- src/app/shared/game-engine/turn/turn.service.spec.ts
+++ src/app/shared/game-engine/turn/turn.service.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from "@angular/core/testing";
-import { playerFactory, PlayerType, TURN_STATE_STORE } from "./turn.model";
+import {
+  playerFactory,
+  Players,
+  PlayerType,
+  TURN_STATE_STORE
+} from "./turn.model";
 import { TurnService } from "./turn.service";
 import { TurnStore } from "./turn.store";
 
 describe("TurnServiceService", () => {
   const player1 = playerFactory("Player 1", PlayerType.Human);
   const player2 = playerFactory("Player 2", PlayerType.Artificial);
+  let service: TurnService;
 
   beforeEach(() =>
     TestBed.configureTestingModule({
@@ -20,7 +26,16 @@ describe("TurnServiceService", () => {
   );
 
   it("should be created", () => {
-    const service: TurnService = TestBed.get(TurnService);
+    service = TestBed.get(TurnService);
     expect(service).toBeTruthy();
+  });
+
+  describe("players$", () => {
+    it("should return the players", done => {
+      service.players$.subscribe((players: Players) => {
+        expect(players).toEqual([player1, player2]);
+        done();
+      });
+    });
   });
 });
