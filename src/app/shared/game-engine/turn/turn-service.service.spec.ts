8732ed6c348000c7539909e1e014ea5042a6c6be
Jari Möllenbernd <jari@moellenbernd.de>
2019-07-12 18:28:04 +0200

    fix: failing test

--- src/app/shared/game-engine/turn/turn-service.service.spec.ts
+++ src/app/shared/game-engine/turn/turn-service.service.spec.ts
@@ -1,11 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { TurnServiceService } from "./turn-service.service";
 
-import { TurnServiceService } from './turn-service.service';
+describe("TurnServiceService", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [TurnServiceService]
+    })
+  );
 
-describe('TurnServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
+  it("should be created", () => {
     const service: TurnServiceService = TestBed.get(TurnServiceService);
     expect(service).toBeTruthy();
   });
